8cc43bf0ecced75a15b2db76e04837da8968c821
Dimon-GDA <dimon.gda@gmail.com>
2013-03-04 22:47:21 +0400

    Initial Commit

---
+++ CoreDataTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreDataTest
+//
+//  Created by admin on 3/4/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CoreDataTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDataTest
+//
+//  Created by admin on 3/4/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
